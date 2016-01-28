d0ab02fac4f116ada4f0a43840de222fc88bafd1
German Tischler <tischler@mpi-cbg.de>
2016-01-28 13:21:27 +0100

    add filtering options

--- src/pileupfastacombine.cpp
+++ src/pileupfastacombine.cpp
@@ -27,6 +27,55 @@ std::string readline(std::istream & in)
 	return line;
 }
 
+struct PileupLine
+{
+	int64_t refpos;
+	int64_t refdif;
+	std::vector< std::pair<char,uint64_t> > pilefreq;
+
+	PileupLine(std::string const & s)
+	{
+		std::deque<std::string> const tokens = libmaus2::util::stringFunctions::tokenize(s,std::string("\t"));
+
+		if ( tokens.size() >= 2 )
+		{
+			refpos = atol(tokens[0].c_str());
+			refdif = atol(tokens[1].c_str());
+
+			for ( uint64_t i = 2; i < tokens.size(); ++i )
+			{
+				std::string const & sp = tokens[i];
+
+				std::deque<std::string> const subtokens = libmaus2::util::stringFunctions::tokenize(sp,std::string(","));
+				if ( subtokens.size() == 2 && subtokens[0].size() == 1 )
+				{
+					std::string const & base = subtokens[0];
+					std::string const & snum = subtokens[1];
+					int64_t const num = atol(snum.c_str());
+					pilefreq.push_back(std::pair<char,uint64_t>(base[0],num));
+				}
+			}
+		}
+	}
+
+	uint64_t getDepth() const
+	{
+		uint64_t depth = 0;
+		for ( uint64_t i = 0; i < pilefreq.size(); ++i )
+			depth += pilefreq[i].second;
+		return depth;
+	}
+};
+
+std::ostream & operator<<(std::ostream & out, PileupLine const & PL)
+{
+	out << "PileupLine(" << PL.refpos << "," << PL.refdif;
+	for ( uint64_t i = 0; i < PL.pilefreq.size(); ++i )
+		out << ",(" << PL.pilefreq[i].first << "," << PL.pilefreq[i].second << ")";
+	out << ")";
+	return out;
+}
+
 int main(int argc, char * argv[])
 {
 	try
@@ -35,6 +84,8 @@ int main(int argc, char * argv[])
 		std::string const multifn = arg[0];
 		std::string const pileAfn = arg[1];
 		std::string const pileBfn = arg[2];
+		bool const filter = arg.argPresent("f");
+		int64_t const freqfilter =  arg.uniqueArgPresent("F") ? atol(arg["F"].c_str()) : 0;
 
 		libmaus2::aio::InputStreamInstance multiISI(multifn);
 		libmaus2::fastx::StreamFastAReaderWrapper SFARW(multiISI);
@@ -76,7 +127,26 @@ int main(int argc, char * argv[])
 			{
 				std::string const linea = readline(pileAISI);
 				std::string const lineb = readline(pileBISI);
-				std::cout << linea << "::" << lineb << std::endl;
+
+				PileupLine pilea(linea);
+				PileupLine pileb(lineb);
+
+				#if 0
+				std::cerr << pilea << std::endl;
+				std::cerr << pileb << std::endl;
+				#endif
+
+				int64_t const maxdepth = std::max(pilea.getDepth(),pileb.getDepth());
+
+				bool const same =
+					pilea.pilefreq.size() == 1
+					&&
+					pileb.pilefreq.size() == 1
+					&&
+					pilea.pilefreq[0].first == pileb.pilefreq[0].first;
+
+				if ( ((! same) || (! filter)) && (maxdepth >= freqfilter) )
+					std::cout << linea << "::" << lineb << std::endl;
 			}
 		}
 	}
